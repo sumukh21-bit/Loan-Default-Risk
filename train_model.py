609cc9122b296781504bee29dffea90ae5ff0dcb
sumukh21-bit <sumukh.jagirdar@gmail.com>
2025-12-29 09:12:30 -0500

    Removed testing stuff

--- train_model.py
+++ train_model.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from data import df
 import joblib
-import pandas as pd
+
 
 
 
@@ -22,40 +22,11 @@ model = Pipeline([
 
 model.fit(x_train,y_train)
 
-dummy_model = {
-  "loan_amnt": 15000,
-  "term": 36,
-  "int_rate": 13.5,
-  "installment": 510,
-  "emp_length": 5,
-  "annual_inc": 65000,
-  "verification_status": 1,
-  "dti": 18.2,
-  "delinq_2yrs": 0,
-  "inq_last_6mths": 1,
-  "open_acc": 7,
-  "pub_rec": 0,
-  "revol_bal": 12000,
-  "revol_util": 45.3,
-  "total_acc": 18,
 
-  "grade_B": True,
-  "grade_C": False,
-  "grade_D": False,
-  "grade_E": False,
-  "grade_F": False,
-  "grade_G": False,
-
-  "home_ownership_MORTGAGE": True,
-  "home_ownership_NONE": False,
-  "home_ownership_OTHER": False,
-  "home_ownership_OWN": False,
-  "home_ownership_RENT": False
-}
 
 y_pred_proba = model.predict_proba(x_test)[:, 1]
 
-predict_to_dummy = model.predict(dummy_model)
+
 roc = roc_auc_score(y_test, y_pred_proba)
 print("ROC-AUC:", roc)
 
